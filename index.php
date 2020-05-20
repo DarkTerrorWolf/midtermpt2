153b6abd3439984ec1b1e274a8affc2eb84f7961
Tylor Schlosser <tschlosser@mail.greenriver.edu>
2020-05-20 15:59:22 -0400

    Summery added and validation created

--- index.php
+++ index.php
@@ -16,18 +16,33 @@ $f3->route('GET /', function () {
 });
 
 $f3->route('GET|POST /form', function ($f3) {
-
-    $f3->set('output',array("This Midterm is easy. ","This is a bit of a challenge. ","I dont know how im going to pass! "));
-    //if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    //}
+    $output = array(" This Midterm is easy"," This is a bit of a challenge"," I dont know how im going to pass! ");
+    $f3->set('output',$output);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+        $input = $_POST['out'];
+        $name = $_POST["name"];
+        if(empty($name)){
+            $f3->set('errors["name"]', "Please enter your name");
+        }
+        if(!isset($input)&& !in_array($input,$output)){
+            $f3->set('errors["message"]', "Please select a message");
+        }
+        if (empty( $f3->get('errors'))){
+            session_start();
+            $_SESSION['name']=$name;
+            $_SESSION['message'] = $input;
+            $f3->reroute('summary');
+        }
+    }
     $view = new Template();
     echo $view->render("views/form.html");
 });
 
 $f3->route('GET /summary', function ($f3) {
-
-}
-);
+    $view = new Template();
+    echo $view->render("views/summary.html");
+    session_destroy();
+});
 
 $f3->run();
